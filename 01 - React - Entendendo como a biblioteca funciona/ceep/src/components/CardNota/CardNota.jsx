c464b82fa91c4a2e4d3407f33e5287014e6f1a59
GabrielRioo <gabrielpaivario@gmail.com>
2021-03-21 15:10:20 -0300

    add style and index.js to import components

--- 01 - React - Entendendo como a biblioteca funciona/ceep/src/components/CardNota/CardNota.jsx
+++ 01 - React - Entendendo como a biblioteca funciona/ceep/src/components/CardNota/CardNota.jsx
@@ -5,10 +5,10 @@ class CardNota extends Component {
   render() {
     return (
       <section className="card-nota">
-        <header>
-          <h3>Titulo</h3>
+        <header className="card-nota_cabecalho">
+          <h3 className="card-nota_titulo">Título</h3>
         </header>
-        <p>Escreva sua anotação</p>
+        <p className="card-nota_texto">Escreva sua nota</p>
       </section>
     );
   }
