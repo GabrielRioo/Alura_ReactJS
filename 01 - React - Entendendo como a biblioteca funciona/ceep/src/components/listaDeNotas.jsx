2d67449d5ca229e6cba9f13a00a364edb0f98a21
GabrielRioo <gabrielpaivario@gmail.com>
2021-03-21 02:07:21 -0300

    add css

--- 01 - React - Entendendo como a biblioteca funciona/ceep/src/components/listaDeNotas.jsx
+++ 01 - React - Entendendo como a biblioteca funciona/ceep/src/components/listaDeNotas.jsx
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import CardNota from "./CardNota/CardNota";
 
-export default class ListaDeNotas extends Component {
-    render() {
-        return (
-            <ul>
-                <li>
-                <section>
-                    <header>
-                    <h3>Titulo</h3>
-                    </header>
-                    <p>Escreva sua anotação</p>
-                </section>
-                </li>
-            </ul>
-        );
-    }
+class ListaDeNotas extends Component {
+  render() {
+    return (
+      <ul>
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categorias, index) => {
+          return (
+            <li key={index}>
+              <div>{categorias}</div>
+              <CardNota />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
+
+export default ListaDeNotas;
